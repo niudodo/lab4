8ab7815c25156bd213b0955c073e8be4edd1bb67
shz310@ucsd.edu <shz310@ucsd.edu>
2019-01-31 10:46:52 -0800

    first draft

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,20 @@
  */
 
 exports.view = function(req, res){
-  res.render('index');
+  res.render("index", {
+  	"projects": [
+  {	"name": "Calloge", 
+  	"image": "FullSizeRender.jpg",
+  	"id":"project1"
+  },{
+  	"name":"International Day Poster",
+	"image":"poster2.jpg",
+	"id":"project2"
+  },{
+  	"name":"WaterColor",
+  	"image":"value-7.jpg",
+  	"id":"project3"
+  }
+  	]
+  });
 };
